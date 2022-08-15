b83122a0bafdb0759a056b2613fce94c77bdfd52
WON Seoyoung <1manysun@hanmail.net>
2022-08-15 18:38:11 +0900

    전체 조정

---
+++ projects/script/project1.js
@@ -0,0 +1,27 @@
+// menu move
+$(".menuOverlay").on("click", function () {
+  $(this).toggleClass("open");
+});
+
+// hamburger menu
+$("#menu_btn").on("click", function () {
+  $(this).toggleClass("on");
+});
+
+// gnb
+$("#menu_btn").on("click", (e) => {
+  e.preventDefault();
+  $("#gnb").slideToggle();
+});
+
+// gnb li 클릭후 원래대로 돌아오게
+const menuBtn = document.querySelector("#menu_btn");
+const gnbLi = document.querySelectorAll("li");
+
+gnbLi.forEach((gnbLi) => {
+  gnbLi.addEventListener("click", (event) => {
+    // Close menu
+    menuBtn.classList.remove("on");
+    $("#gnb").slideUp();
+  });
+});
--- script/script.js
+++ script/script.js
@@ -1,9 +1,6 @@
-///////// menu
-
-// gnb
-$("#menu_btn").on("click", (e) => {
-  e.preventDefault();
-  $("#gnb").slideToggle();
+// menu move
+$(".menuOverlay").on("click", function () {
+  $(this).toggleClass("open");
 });
 
 // hamburger menu
@@ -11,10 +8,35 @@ $("#menu_btn").on("click", function () {
   $(this).toggleClass("on");
 });
 
-// project
-// let swiper = new Swiper(".mySwiper", {
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//     hide: true,
-//   },
+// gnb
+$("#menu_btn").on("click", (e) => {
+  e.preventDefault();
+  $("#gnb").slideToggle();
+});
+
+// gnb li 클릭후 원래대로 돌아오게
+const menuBtn = document.querySelector("#menu_btn");
+const gnbLi = document.querySelectorAll("li");
+
+gnbLi.forEach((gnbLi) => {
+  gnbLi.addEventListener("click", (event) => {
+    // Close menu
+    menuBtn.classList.remove("on");
+    $("#gnb").slideUp();
+  });
+});
+
+// project (slide ing + tab menu)
+$("#pjBtns li").on("click", function () {
+  let idx = $(this).index();
+  $("#pjImg_wrap").animate({ marginLeft: `${-100 * idx}%` });
+});
+
+// const pjBtn = document.querySelectorAll("#pjBtns li");
+
+// pjBtn.forEach((pjBtn) => {
+//   pjBtn.classList.remove("on");
+//   pjBtn.addEventListener("click", () => {
+//     pjBtn.classList.add("on");
+//   });
 // });
