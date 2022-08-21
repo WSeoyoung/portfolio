c525f1df26759d610054918b4627d358f9d78685
WON Seoyoung <1manysun@hanmail.net>
2022-08-22 05:23:25 +0900

    켈로그 일단 완

--- kellogg/script.js
+++ kellogg/script.js
@@ -1,3 +1,15 @@
+// nav sticky
+const nav = document.querySelector("#navWrap");
+const header = document.querySelector("header");
+
+addEventListener("scroll", () => {
+  if (scrollY > header.offsetTop - 50) {
+    nav.classList.add("sticky");
+  } else if (scrollY <= header.offsetTop - 50) {
+    nav.classList.remove("sticky");
+  }
+});
+
 // slide 좌우
 
 let slideCount = $(".slideImg").length;
@@ -13,3 +25,26 @@ setInterval(() => {
   slidePosition = currentIndex * -100 + "vw";
   $(".slideList").animate({ left: slidePosition }, 400);
 }, 3000);
+
+// better pop mousemove
+const pop1 = document.querySelector(".pop1");
+const pop2 = document.querySelector(".pop2");
+const pop3 = document.querySelector(".pop3");
+const pop4 = document.querySelector(".pop4");
+
+addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  pop1.style.top = `${x / -15}px`;
+  pop1.style.left = `${y / -20}px`;
+
+  pop2.style.top = `${x / 40}px`;
+  pop2.style.left = `${y / 0}px`;
+
+  pop3.style.top = `${x / -40}px`;
+  pop2.style.right = `${y / 50}px`;
+
+  pop4.style.top = `${x / 10}px`;
+  pop4.style.right = `${y / -20}px`;
+});
