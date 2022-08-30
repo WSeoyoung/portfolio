340a467c004b93f2681571e55dad932722bf31cd
WON Seoyoung <1manysun@hanmail.net>
2022-08-31 05:24:21 +0900

    ddp swiper부분 진행중

--- ddp/script.js
+++ ddp/script.js
@@ -1,5 +1,41 @@
-// mobile nav
-
-$(".toggleBtn").on("click", () => {
-  $(".toggleBtn").toggleClass("active");
+// hamburger menu
+$(".menu_btn").on("click", function (e) {
+  e.preventDefault();
+  $(this).toggleClass("on");
 });
+
+// mobile nav
+$(".btn_open").click((e) => {
+  e.preventDefault();
+  $(".overlay").fadeToggle(200);
+  $(this).addClass("open");
+});
+
+$(".btn_close").click((e) => {
+  e.preventDefault();
+  $(".overlay").fadeToggle(200);
+  $(this).removeClass("open");
+});
+
+// nav scroll shadow
+const nav = document.querySelector("nav");
+
+addEventListener("scroll", () => {
+  if (scrollY > 10) {
+    nav.classList.add("shadow");
+  } else {
+    nav.classList.remove("shadow");
+  }
+});
+
+// gnb li 클릭시 menu 화면 꺼짐 => 왜 안될까...
+// const gnbLis = document.querySelectorAll("#gnb li");
+// const menu = document.querySelector(".overlay");
+
+// gnbLis.forEach((gnbLis) => {
+//   gnbLis.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     // Close menu
+//     $(menu).removeClass("open");
+//   });
+// });
