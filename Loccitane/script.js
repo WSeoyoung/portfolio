ce18f8106b3b07e81360039859e7122757b515cf
WON Seoyoung <1manysun@hanmail.net>
2022-10-27 17:56:39 +0900

    록시땅 수정

--- Loccitane/script.js
+++ Loccitane/script.js
@@ -16,74 +16,27 @@ toggleBtn.addEventListener("click", (e) => {
   pcNav.classList.toggle("on");
 });
 
-///////////////////////////////////////////////////////////////
+// 스크롤 이벤트
 
-// 리미티드 txt
+const nav = document.querySelector("nav");
+const slideTxt = document.querySelector("#slide_txt");
 
-const lmtTitles = document.querySelectorAll(".lmt_txt_title");
-const lmtBtns = document.querySelectorAll(".carousel");
-const lmtNum = document.querySelectorAll(".lmtnum");
-let num = 1;
-
-lmtTitles.forEach((lmtTitles) => {
-  lmtTitles.addEventListener("click", (e) => {
-    if (num) {
-      e.currentTarget.style.borderBottom = "2px solid #ffcb00";
-      e.currentTarget.style.paddingBottom = "10px";
-      num = 0;
-    } else {
-      e.currentTarget.style.borderBottom = "none";
-      e.currentTarget.style.paddingBottom = "10px";
-      num = 1;
-    }
-  });
+addEventListener("scroll", () => {
+  if (scrollY > slideTxt.offsetTop - 180) {
+    nav.classList.add("style");
+  } else if (scrollY <= slideTxt.offsetTop - 180) {
+    nav.classList.remove("style");
+  }
 });
-
-lmtBtns.forEach((lmtBtns) => {
-  lmtBtns.addEventListener("click", (e) => {
-    if (num) {
-      e.currentTarget.style.transform = "rotate(180deg)";
-      e.currentTarget.classList.toggle("on");
-      num = 0;
-    } else {
-      e.currentTarget.style.transform = "rotate(-180deg)";
-      e.currentTarget.classList.toggle("off");
-      num = 1;
-    }
-  });
-});
-
-lmtBtns.forEach((lmtBtns) => {
-  lmtBtns.addEventListener("click", (e) => {
-    e.currentTarget.style.transform = "rotate(180deg)";
-    lmtBtns.classList.toggle("on");
-  });
-});
-
-/*
-let num = 1;
-
-
-*/
-
-// const btnEvent = (e) => {
-//   const current = e.currentTarget;
-
-//   lmtBtns.forEach((lmtBtns) => lmtBtns.classList.remove("active"));
-//   current.parentNode.classList.add("active");
-// };
 
 ///////////////////////////////////////////////////////////////
 
 // // 사이드 메뉴 서브메뉴
 
-// const sub1 = document.querySelector(".sub1");
-// const sub2 = document.querySelector(".sub2");
-// const sub3 = document.querySelector(".sub3");
-// const sub4 = document.querySelector(".sub4");
-// const sub5 = document.querySelector(".sub5");
-// const subMenu = document.querySelector(".sub_menu");
-
-// sub1.addEventListener("click", () => {
-//   subMenu.style.display = "block";
-// });
+$("#gnb>li>a")
+  .mouseover(function () {
+    $(this).find(".sub_menu").stop().slideDown(500);
+  })
+  .mouseout(function () {
+    $(this).find(".sub_menu").stop().slideUp(500);
+  });
